4017c1a6da201ea54f952eec765e3e066471ba55
Abhay gupta <129143621+Abs6187@users.noreply.github.com>
2024-08-30 00:48:02 +0530

    Create Easy_Crazy_Numbers.java

---
+++ POTD/Date 30 Aug 2024/Easy_Crazy_Numbers.java
@@ -0,0 +1,49 @@
+import java.util.* ;
+
+import java.io.*; 
+
+import java.util.ArrayList;
+
+ 
+
+public class Solution {
+
+    public static ArrayList<ArrayList<Integer>> numberPattern(int n) {
+
+        ArrayList<ArrayList<Integer>> pattern = new ArrayList<>(); 
+
+        int num = 1;
+
+        for (int i = 1; i <= n; i++) 
+
+        {   
+
+            ArrayList<Integer> row = new ArrayList<>();
+
+            for(int j=1; j<=n-i; j++)
+
+            {
+
+                row.add(-1);
+
+            }
+
+            for (int j = 1; j <= i; j++) 
+
+            {
+
+              row.add(num);
+
+               num = (num % 9) + 1; // Restart from 1 after reaching 9
+
+            }
+
+           pattern.add(row);
+
+       }
+
+       return pattern;
+
+    }
+
+}
