c735658ce94bc229f3618c4fe64910c7a28c66a1
Abhay gupta <129143621+Abs6187@users.noreply.github.com>
2024-09-09 00:33:58 +0530

    Update MODERATE Rotate Linked List.py

--- POTD/DATE 09 SEP/MODERATE Rotate Linked List.py
+++ POTD/DATE 09 SEP/MODERATE Rotate Linked List.py
@@ -1,26 +1,24 @@
-def nthNode(head:Node , pos) -> Node:
-    temp , cnt = head , 1
-
-    while temp.next:
-        if cnt==pos: return temp
-        cnt+=1
-        temp = temp.next
-    return temp
-    
 def rotate(head: Node, k: int) -> Node:
-    if head is None or k==0: return head
-
-    tail , lens = head , 1
-    while tail.next is not None: # finding the length of the linked list
-        lens+=1
-        tail = tail.next
-
-    if k%lens==0: return head
-
-    k = k % lens
-    LastNode = nthNode(head,lens-k)
-    tail.next = head
-    head = LastNode.next
-    LastNode.next = None
-    
+    # Write your code here.
+    if head is not None and head.next is not None:
+        stack = []
+        n = 1
+        last = head
+        while last.next is not None:
+            n+=1
+            stack.append(last)
+            last = last.next
+        k = k%n
+        if k!=0:
+            i = 0
+            prev = last
+            cur = last
+            while i<k:
+                i+=1
+                prev = cur
+                cur = stack.pop(n-2)
+                n-=1
+            cur.next = None
+            last.next = head
+            head = prev
     return head
