bfecb1be69859c220004c516ef8774eeeb660a9d
Abhay gupta <129143621+Abs6187@users.noreply.github.com>
2024-09-23 00:05:53 +0530

    Create MODERATE LCA of Two Nodes In A BST.cpp

---
+++ POTD/DATE 23 SEP/MODERATE LCA of Two Nodes In A BST.cpp
@@ -0,0 +1,28 @@
+TreeNode *LCAinaBST(TreeNode *root, TreeNode *P, TreeNode *Q)
+
+{
+
+   // base case 
+
+   if(root == NULL)
+
+    return NULL;
+
+ 
+
+    if(root -> data < P->data && root -> data < Q -> data)
+
+      return LCAinaBST(root-> right, P, Q);
+
+ 
+
+        if(root -> data > P -> data && root -> data > Q-> data)
+
+          return LCAinaBST(root -> left, P, Q);
+
+ 
+
+          return root;
+
+}
+
